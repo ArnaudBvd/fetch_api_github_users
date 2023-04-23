f1b87b43814536ddbc06f1607eec75a1cde12e7c
ArnaudBvd <bravard.arnaud@gmail.com>
2023-04-23 13:10:17 +0200

    ajout d'une DIV en html et ajout du script en js

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-fetch("https://api.github.com/users/")  // Permet de facilement récupérer des ressources et de faire des requêtes
+// function getUsers() {
+    btn.onclick = () => {
+        fetch("https://api.github.com/users/" + champ.value) // Permet de facilement récupérer des ressources et de faire des requêtes
 
-/* Permet de récupérer la réponse de notre fetch
-on transforme ensuite cette réponse en '.json' donc en objet afin qu'il puisse être interprétable par notre navigateur */
-.then(query_answer => query_answer.json())
+            /* Permet de récupérer la réponse de notre fetch
+    on transforme ensuite cette réponse en '.json' donc en objet afin qu'il puisse être interprétable par notre navigateur */
+            .then(query_answer => query_answer.json())
 
-// Deuxième '.then' qui permet maintenant de lire le '.json'
-.then(query_answer2 => )
+            // Deuxième '.then' qui permet maintenant de lire le '.json'
+            .then(data => {
+                output.textContent = ""; // permet de se vider lors d'une nouvelle requête 
+                output.textContent = `Compte de ${data.name}`;
+                const img = document.createElement('img');
+                img.src = data.avatar_url;
+                output.appendChild(img);
+            })
+    }
+// }
+
